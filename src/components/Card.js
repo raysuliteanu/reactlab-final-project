1a12f57c7844163eec4ba9f47388ea62529228ad
Ray Suliteanu <raysuliteanu@gmail.com>
2024-03-14 11:58:47 -0700

    initial commit
    * skeleton from project files
    * implemented Header and Card components
    * added ReactBuddy plugin

---
+++ src/components/Card.js
@@ -0,0 +1,30 @@
+import {Heading, HStack, Image, Text, VStack} from "@chakra-ui/react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+const Card = ({title, description, imageSrc}) => {
+    // Implement the UI for the Card component according to the instructions.
+    // You should be able to implement the component with the elements imported above.
+    // Feel free to import other UI components from Chakra UI if you wish to.
+    return (
+        <VStack
+            align="left"
+            borderRadius="xl"
+            backgroundColor="white"
+            textColor="black"
+        >
+            <Image src={imageSrc} borderRadius="xl"/>
+            <VStack p="5" align="left">
+                <Heading>{title}</Heading>
+                <Text>{description}</Text>
+                <HStack verticalAlign="center">
+                    <Text>See more</Text>
+                    <FontAwesomeIcon icon={faArrowRight}/>
+                </HStack>
+            </VStack>
+        </VStack>
+    );
+};
+
+export default Card;
