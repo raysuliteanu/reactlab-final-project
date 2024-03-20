7c5548aef93876e02fe665c7465c935c1a0dac92
Ray Suliteanu <raysuliteanu@gmail.com>
2024-03-19 17:33:50 -0700

    start adding some unit tests

---
+++ src/components/Header.test.js
@@ -0,0 +1,14 @@
+import {render, screen} from '@testing-library/react';
+import Header from "./Header";
+
+test('renders projects link in header', () => {
+    render(<Header/>);
+    const linkElement = screen.getByText(/projects/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('renders contact me link in header', () => {
+    render(<Header/>);
+    const linkElement = screen.getByText(/contact me/i);
+    expect(linkElement).toBeInTheDocument();
+});
