4416d4e7f3a87c0d2570f6c3a1917d52268ac681
Ray Suliteanu <raysuliteanu@gmail.com>
2024-03-27 11:41:51 -0700

    DRY

--- src/components/ContactMeSection.test.js
+++ src/components/ContactMeSection.test.js
@@ -4,26 +4,23 @@ import {AlertProvider} from "../context/alertContext";
 import {ChakraProvider} from "@chakra-ui/react";
 
 describe("'Contact me' tests", () => {
-    test('has form', () => {
-        render(
+    const contactMeSection = () => {
+        return (
             <ChakraProvider>
                 <AlertProvider>
                     <ContactMeSection/>
                 </AlertProvider>
             </ChakraProvider>
         );
+    }
+    test('has form', () => {
+        render(contactMeSection());
         const formElement = screen.getByRole("form");
         expect(formElement).toBeInTheDocument();
     });
 
     test('form submit is disabled initially', () => {
-        render(
-            <ChakraProvider>
-                <AlertProvider>
-                    <ContactMeSection/>
-                </AlertProvider>
-            </ChakraProvider>
-        );
+        render(contactMeSection());
 
         const submitButton = screen.getByRole('button');
         expect(submitButton).toBeDisabled();
